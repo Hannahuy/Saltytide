a7d114283032c005e214f44d71ee97583c36f75f
Hannahuy <hanyu20011010@icloud.com>
2024-07-03 11:04:02 +0800

    点击展开详情

--- src/components/topButton/Monitoring.vue
+++ src/components/topButton/Monitoring.vue
@@ -52,6 +52,27 @@
       <div id="WaterCharts"></div>
     </div>
   </div>
+  <div class="right-box" v-if="Devicebox">
+    <div class="top-leftbox-title">
+      <span>设备信息</span>
+      <span>Device information</span>
+      <img class="closeimg" src="../../assets/image/close.png" alt="" @click="close" />
+    </div>
+    <div class="right-box-img">
+      <img :src="Deviceimg" alt="" class="Deviceimgbox">
+    </div>
+    <div class="right-box-content">
+      <div class="right-box-content-top">
+        <span style="margin-right: 10px;">设备类型：{{ BDeviceType }}</span>
+        <span style="margin-right: 10px;">设备名称：{{ BDeviceName }}</span>
+        <span>是否在线：{{ BOnlineStatus }}</span>
+      </div>
+      <div class="right-box-content-bottom">
+        <span style="margin-right: 10px;">经度：{{ BLongitude }}</span>
+        <span>纬度：{{ BLatitude }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -59,31 +80,49 @@ import { ref, onMounted, onBeforeUnmount } from "vue";
 import * as echarts from "echarts";
 import tabledataJson from "/public/data/实时监测.json";
 import { callUIInteraction, addResponseEventListener, } from "../../module/webrtcVideo/webrtcVideo.js";
+import salinity from '../../assets/img/盐度监测设备.png';
+import flowrate from '../../assets/img/流量监测设备.png';
+import waterlevel from '../../assets/img/水位监测设备.png';
+import pumpingstation from '../../assets/img/泵站监测设备.png';
 
+const BDeviceType = ref('');
+const BDeviceName = ref('');
+const BOnlineStatus = ref('');
+const BLongitude = ref('');
+const BLatitude = ref('');
+const Devicebox = ref(false);
+const Deviceimg = ref()
 const dataxAxis = ref(['00:00', '01:15', '02:30', '03:45', '05:00', '06:15', '07:30', '08:45', '10:00', '11:15', '12:30', '13:45', '15:00', '16:15', '17:30', '18:45', '20:00', '21:15', '22:30', '23:45'])
 const dataseries = ref([20, 30, 25, 35, 40, 45, 50, 48, 45, 40, 35, 30, 28, 30, 32, 35, 38, 40, 42, 40, 38, 36, 34, 32])
 const echartsname = ref('水位监测')
 const myHandleResponseFunction = (data) => {
-  console.log(data);
-  echartsname.value = data;
-  switch (data) {
+  const jsonData = JSON.parse(data);
+  echartsname.value = jsonData.DeviceType;
+  switch (jsonData.DeviceType) {
     case '泵站监测':
       dataseries.value = [120, 130, 125, 135, 140, 145, 150, 148, 145, 140, 135, 130, 128, 130, 132, 135, 138, 140, 142, 140, 138, 136, 134, 132];
-      initWaterChart();
+      Deviceimg.value = pumpingstation;
       break;
     case '流量监测':
       dataseries.value = [120, 130, 125, 135, 140, 145, 150, 148, 145, 140, 135, 130, 128, 130, 132, 135, 138, 140, 142, 140, 138, 136, 134, 132];
-      initWaterChart();
+      Deviceimg.value = flowrate;
       break;
     case '水位监测':
       dataseries.value = [20, 30, 25, 35, 40, 45, 50, 48, 45, 40, 35, 30, 28, 30, 32, 35, 38, 40, 42, 40, 38, 36, 34, 32];
-      initWaterChart();
+      Deviceimg.value = waterlevel;
       break;
     case '盐度监测':
       dataseries.value = [4.4, 3.4, 3.1, 5.6, 7.5, 8.5, 9.5, 10.4, 11.4, 9.8, 7.5, 7.9, 7.1, 6.4, 6.2, 5.8, 5.4, 5.1, 4.7, 4.5, 3.9, 3.5, 3.4, 3.6];
-      initWaterChart();
+      Deviceimg.value = salinity;
       break;
   }
+  initWaterChart();
+  Devicebox.value = true;
+  BDeviceType.value = jsonData.DeviceType;
+  BDeviceName.value = jsonData.DeviceName;
+  BOnlineStatus.value = jsonData.OnlineStatus;
+  BLongitude.value = jsonData.Longitude;
+  BLatitude.value = jsonData.Latitude;
 };
 const tableData = ref([]);
 // 获取表格数据
@@ -180,7 +219,7 @@ const initSalinityChart = () => {
       textStyle: {
         color: '#b7cffc',
       },
-      itemGap:20, // 间距
+      itemGap: 20, // 间距
     },
     series: [
       {
@@ -199,7 +238,7 @@ const initSalinityChart = () => {
               textStyle: {
                 color: '#b7cffc',
                 fontWeight: 'bolder',
-                fontSize:14
+                fontSize: 14
               }
             },
           }
@@ -218,7 +257,10 @@ const initSalinityChart = () => {
   };
   salinityChartInstance.setOption(options);
 };
-
+// 关闭设备信息
+const close = () => {
+  Devicebox.value = false;
+}
 onMounted(() => {
   initWaterChart();
   initSalinityChart();
@@ -247,6 +289,20 @@ onBeforeUnmount(() => {
   position: absolute;
   top: 11%;
   left: 10px;
+  padding: 20px;
+  overflow: hidden;
+}
+
+.right-box {
+  width: 500px;
+  height: 35%;
+  min-height: 339px;
+  background-image: url("../../assets/image/框-bg.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  position: absolute;
+  bottom: 20px;
+  right: 10px;
   padding: 20px;
   overflow: hidden;
 }
@@ -382,4 +438,45 @@ onBeforeUnmount(() => {
   font-family: YouSheBiaoTiHei;
   left: 30px;
 }
+
+.closeimg {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  right: 25px;
+  top: 25px;
+  cursor: pointer;
+}
+
+.Deviceimgbox {
+  width: 100%;
+  height: 100%;
+}
+
+.right-box-img {
+  width: 100%;
+  height: 200px;
+  margin-top: 10px;
+}
+.right-box-content {
+  margin-top: 10px;
+  height: 90px;
+  color: #b7cffc;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+
+.right-box-content-top,
+.right-box-content-bottom {
+  width: 100%;
+  display: flex;
+}
+
+.right-box-content-top span,
+.right-box-content-bottom span {
+  display: inline-block;
+  width: 160px;
+}
+
 </style>
