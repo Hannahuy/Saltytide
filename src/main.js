b1c3706498772a76a9dc6cbd36ba755f0ac5d6f8
yetliuxain <1067199094@qq.com>
2024-07-05 14:38:08 +0800

    js 后端接口

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import axios from 'axios'
 
-axios.get('./public/config.json').then((res) => {
-  window.VITE_APP_BASE_API = res.data.VITE_APP_BASE_API;
-  const app = createApp(App)
-  app.use(ElementPlus, {
-      locale: zhCn,
-    })
-  app.use(routes)
-  app.mount('#app')
+const VITE_APP_BASE_API = 'http://192.168.0.124:5000/';
+window.VITE_APP_BASE_API = VITE_APP_BASE_API;
+console.log(window.VITE_APP_BASE_API)
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
 })
+app.use(routes)
+app.mount('#app')
